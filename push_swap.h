0f4dcb0669c5ee8eca82b50e5440bf94f4824b96
eduardosalgado12 <eduardosalgado@outlook.pt>
2026-05-20 17:06:40 +0100

    Add files via upload

--- push_swap.h
+++ push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: khooftma <khooftma@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: edsalgad <edsalgad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 15:21:04 by khooftma          #+#    #+#             */
-/*   Updated: 2026/05/19 15:25:38 by khooftma         ###   ########.fr       */
+/*   Updated: 2026/05/20 16:18:49 by edsalgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 # define PUSH_SWAP_H
 
 # include <unistd.h>
+
+typedef struct s_stack
+{
+    int             value;       // O número original recebido por parâmetro
+    int             index;       // A posição dele se estivesse ordenado (0 a N-1)
+    struct s_stack  *next;       // Ponteiro para o próximo nó abaixo na pilha
+    struct s_stack  *prev;       // Ponteiro para o nó anterior acima na pilha
+}   t_stack;
+
+int	ft_lstsize(t_stack *lst);
 
 #endif
