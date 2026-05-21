72706ed713bd43cac66a25d373c254d8f0a5cc23
Khooftman <k.hooftman@gmail.com>
2026-05-21 12:28:24 +0100

    errors

---
+++ errors.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errors.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khooftma <khooftma@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/20 17:35:38 by khooftma          #+#    #+#             */
+/*   Updated: 2026/05/21 12:26:55 by khooftma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	errors_syntax(char *str)
+{
+	if(!(*str == '+' || *str == '-' || (*str >= '0' && *str <= '9')))
+		return (1);
+	if ((*str == '+' || *str == '-') && !(str[1] >= '0' && str[1] <= '9'))
+		return (1);
+	while (*++str)
+	{
+		if (!(*str >= '0' && *str <= '9'))
+			return (1);
+	}
+	return (0);
+}
+
+int	errors_duplicate(t_stack *a, int n)
+{
+	if (!a)
+		return (0);
+	while (a)
+	{
+		if (a->value == n)
+			return (1);
+		a = a->next;
+	}
+	return (0);
+}
+
+void	free_stack(t_stack **stack)
+{
+	t_stack	*temp;
+	t_stack	*current;
+
+	if (!stack)
+		return ;
+	current = *stack;
+	while (current)
+	{
+		temp = current->next;
+		current->value = 0;
+		free(current);
+		current = temp;
+	}
+	*stack = NULL;
+}
+
+void	free_errors(t_stack	**a)
+{
+	free_stack(a);
+	printf("Error\n");
+	exit(1);
+}
