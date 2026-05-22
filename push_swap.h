efc8c1707b297b15bf5a59d3f84ae8df9c9343d9
Koen Herman D Hooftman <khooftma@c1r14s1.42porto.com>
2026-05-22 15:54:51 +0100

    updates

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: khooftma <khooftma@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 15:21:04 by khooftma          #+#    #+#             */
-/*   Updated: 2026/05/22 11:50:55 by khooftma         ###   ########.fr       */
+/*   Updated: 2026/05/22 15:54:15 by khooftma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,23 +27,30 @@ typedef struct s_stack
     struct s_stack  *prev;       // Ponteiro para o nó anterior acima na pilha
 }   t_stack;
 
+//stack_init
 void	stack_init(t_stack **a, char **argv);
 
+//errors
 int		errors_syntax(char *str);
 int		errors_duplicate(t_stack *a, int n);
 void	free_errors(t_stack	**a);
 
+// split
 char	**ft_split(char const *s, char c);
 
+//commands
 void	swap_a(t_stack **sa);
 bool	stack_sorted(t_stack *sa);
 int		stack_len(t_stack *sa);
 void	swap(t_stack **stack);
-
-
 void	rotate(t_stack **stack);
 void 	reverse_rotate(t_stack **stack);
-
 void	push(t_stack **dst, t_stack **src);
+
+// utils
+int		ft_lstsize(t_stack *lst);
+
+// algorithms
+void	sort_three(t_stack **stack_a);
 
 #endif
