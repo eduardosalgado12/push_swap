75123dc3e1cdc3ad8b79fa7ff2490728094cccf2
Koen Herman D Hooftman <khooftma@c1r7s2.42porto.com>
2026-05-25 15:30:37 +0100

    simple update

--- simple.c
+++ simple.c
@@ -6,44 +6,68 @@
 /*   By: khooftma <khooftma@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/25 09:31:16 by khooftma          #+#    #+#             */
-/*   Updated: 2026/05/25 15:08:41 by khooftma         ###   ########.fr       */
+/*   Updated: 2026/05/25 15:30:06 by khooftma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	find_min_value(t_stack *stack)
+int	get_stack_size(t_stack *stack)
 {
-	int	min;
-	
-	min = stack->value;
-	while (stack)
+	int	count;
+
+	count = 0;
+	while (stack != NULL)
 	{
-		if (stack->value < min)
-			min = stack->value;
+		count++;
 		stack = stack->next;
 	}
-	return (min);
+	return (count);
 }
 
-
-void	selection_sort(t_stack **a, t_stack **b)
+int	get_min_pos(t_stack *stack)
 {
-	int	i;
-	int min;
+	t_stack	*current;
+	int		min_value;
+	int		min_pos;
+	int		i;
 
+	current = stack;
+	min_value = current->value;
+	min_pos = 0;
 	i = 0;
-	while (*a)
+	while (current)
 	{
-		min = find_min_value(*a);
-		while ((*a)->value != min)
-			ra(a);
-		pb(a, b);
+		if (current->value < min_value)
+		{
+			min_value = current->value;
+			min_pos = i;
+		}
+		current = current->next;
 		i++;
 	}
-	while (i > 0)
+	return (min_pos);
+}
+
+void	simple(t_stack **stack_a, t_stack **stack_b)
+{
+	int	pos;
+	int	size;
+
+
+	while (*stack_a)
 	{
-		pa(a, b);
-		i--;
-	}	
+		pos = get_min_pos(*stack_a);
+		size = get_stack_size(*stack_a);
+		if (pos <= size / 2)
+			while (pos-- > 0)
+				ra(stack_a);
+		else
+			while (pos++ < size)
+				rra(stack_a);
+				
+		pb(stack_a, stack_b);
+	}
+	while (*stack_b)
+		pa(stack_a, stack_b);
 }
